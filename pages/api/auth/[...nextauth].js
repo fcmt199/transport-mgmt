932c19a368c106bb4edf269dfb909f84681a2b1c
Flavia Moscoso <flavia.celeste1999@gmail.com>
2023-09-18 12:12:55 -0400

    initial commit next-app

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github'
-//import { MongoDBAdapter } from '@auth/mongodb-adapter'
-//import clientPromise from 'lib/mongodb'
+
 export default NextAuth({
-     //adapter: MongoDBAdapter(clientPromise),
+
     providers: [
             GithubProvider({
                 clientId: process.env.GITHUB_ID,
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { getSession } from "next-auth/react"
+import { pool } from '../../config/db';
 
-export default async function handler(req, res) {
-  const session= await getSession({req})
-  console.log(session)
-  if(!session)
-  return res.status(403).send('forbidden')
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req,res) {
+ 
+    const [rows] = await pool.query('SELECT NOW()')
+ 
+
+    return res.status(200).json(rows[0]['NOW()']);
 }
